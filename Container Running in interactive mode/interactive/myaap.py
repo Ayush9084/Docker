ed2c687e36fe083a485c26eafeca898fda23226c
Ayush Pandey <69528843+Ayush9084@users.noreply.github.com>
2025-11-13 10:19:06 +0530

    Running Container in Interactive Mode

---
+++ Container Running in interactive mode/interactive/myaap.py
@@ -0,0 +1,12 @@
+# Program to find the sum of two numbers (user-defined)
+
+# Taking input from the user
+num1 = float(input("Enter first number: "))
+num2 = float(input("Enter second number: "))
+
+# Calculating sum
+sum = num1 + num2
+
+# Displaying result
+print("The sum of", num1, "and", num2, "is:", sum)
+
